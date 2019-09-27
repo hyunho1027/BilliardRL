8b73911e59fb4f6626902e4f186eb3fdbe186834
이현호 <hyunho1027@khu.ac.kr>
2019-09-28 00:53:53 +0900

    ppo & ddpg update

--- ppo.py
+++ ppo.py
@@ -16,7 +16,7 @@ a_size = 2
 gamma = 0.9
 _lambda = 0.95
 epoch = 10
-lr = 3e-4
+lr = 5e-5
 ppo_eps = 0.2
 n_step = 64
 
@@ -28,7 +28,7 @@ save_path = "./summary/ppo/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 save_interval = 100
 
 
-class Model:
+class Net:
     def __init__(self, name):
         with tf.variable_scope(name):
 
@@ -39,54 +39,55 @@ class Model:
                 self.s_normalize = (self.s - (255.0 / 2)) / (255.0 / 2)
 
                 self.conv1 = tf.layers.conv2d(self.s_normalize, filters=32,
-                                            activation=tf.nn.relu, kernel_size=[8,8],
+                                            activation=tf.nn.leaky_relu, kernel_size=[8,8],
                                             strides=[4,4], padding="SAME")
                 self.conv2 = tf.layers.conv2d(self.conv1, filters=64,
-                                            activation=tf.nn.relu, kernel_size=[4,4],
+                                            activation=tf.nn.leaky_relu, kernel_size=[4,4],
                                             strides=[2,2], padding="SAME")
                 self.conv3 = tf.layers.conv2d(self.conv2, filters=64,
-                                            activation=tf.nn.relu, kernel_size=[3,3],
+                                            activation=tf.nn.leaky_relu, kernel_size=[3,3],
                                             strides=[1,1], padding="SAME")
 
                 self.flat = tf.layers.flatten(self.conv3)
-                self.fc1 = tf.layers.dense(self.flat, 128, tf.nn.relu)
-                self.fc2 = tf.layers.dense(self.fc1, 128, tf.nn.relu)
-                self.fc3 = tf.layers.dense(self.fc2, 128, tf.nn.relu)
+                self.fc1 = tf.layers.dense(self.flat, 128, tf.nn.leaky_relu)
+                self.fc2 = tf.layers.dense(self.fc1, 128, tf.nn.leaky_relu)
+                self.fc3 = tf.layers.dense(self.fc2, 128, tf.nn.leaky_relu)
 
             else:
-                self.fc1 = tf.layers.dense(self.s, 128, tf.nn.relu)
-                self.fc2 = tf.layers.dense(self.fc1, 128, tf.nn.relu)
-                self.fc3 = tf.layers.dense(self.fc2, 128, tf.nn.relu)
+                self.fc1 = tf.layers.dense(self.s, 128, tf.nn.leaky_relu)
+                self.fc2 = tf.layers.dense(self.fc1, 128, tf.nn.leaky_relu)
+                self.fc3 = tf.layers.dense(self.fc2, 128, tf.nn.leaky_relu)
 
             ## Actor
             self.mu = tf.layers.dense(self.fc3, a_size)
             self.log_std = tf.get_variable("log_std", initializer=-0.5*np.ones(a_size, np.float32))
             self.std = tf.exp(clip_b4_exp(self.log_std))
             self.pi = tf.sigmoid(clip_b4_exp(tf.random_normal(tf.shape(self.mu), self.mu, self.std)))
+            self.test_pi = tf.sigmoid(clip_b4_exp(self.mu))
             self.logp = gaussian_likelihood(logit(self.a), self.mu, self.log_std)
             self.logp_pi = gaussian_likelihood(self.pi, self.mu, self.log_std)
 
             ## Critic
-            self.value = tf.layers.dense(self.fc3, 1)
-            self.v = tf.squeeze(self.value, axis=1)
+            self.v = tf.squeeze(tf.layers.dense(clip_b4_exp(self.fc3), 1, tf.nn.tanh), axis=1)
 
 class PPOAgent:
     def __init__(self):
-        self.model = Model("Actor")
+        self.net = Net("Net")
 
         self.adv = tf.placeholder(tf.float32, [None])
         self.ret = tf.placeholder(tf.float32, [None])
         self.logp_old = tf.placeholder(tf.float32, [None])
 
-        self.ratio = tf.exp(self.model.logp - self.logp_old)
-        self.min_adv = tf.where(self.adv > 0, (1.0+ppo_eps)*self.adv, (1.0-ppo_eps)*self.adv)
-        self.pi_loss = -tf.reduce_mean(tf.minimum(self.ratio*self.adv, self.min_adv))
-        self.v_loss = tf.reduce_mean((self.ret-self.model.v)**2)
+        self.ratio = tf.exp(self.net.logp - self.logp_old)
+        self.min_adv = tf.where(self.adv > 0, (1.0 + ppo_eps)*self.adv, (1.0 - ppo_eps)*self.adv)
+        self.pi_loss = - tf.reduce_mean(tf.minimum(self.ratio*self.adv, self.min_adv))
+        self.v_loss = tf.reduce_mean((self.ret - self.net.v)**2)
+
         self.total_loss = self.pi_loss + self.v_loss
         self.train_model = tf.train.AdamOptimizer(lr).minimize(self.total_loss)
 
-        self.approx_kl = tf.reduce_mean(self.logp_old - self.model.logp)
-        self.approx_ent = tf.reduce_mean(-self.model.logp)
+        self.approx_kl = tf.reduce_mean(self.logp_old-self.net.logp)
+        self.approx_ent = tf.reduce_mean(- self.net.logp)
 
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
@@ -98,15 +99,18 @@ class PPOAgent:
         for i in range(epoch):
             _, sub_v_loss, approx_kl, approx_ent = \
                  self.sess.run([self.train_model, self.v_loss, self.approx_kl, self.approx_ent],
-                                feed_dict={self.model.s: s, self.model.s: s, self.model.a: a, self.ret: target, self.adv: adv, self.logp_old: logp_old})
+                                feed_dict={self.net.s: s, self.net.s: s, self.net.a: a, self.ret: target, self.adv: adv, self.logp_old: logp_old})
             v_loss += sub_v_loss
             kl += approx_kl
             ent += approx_ent
         return v_loss, kl, ent
 
-    def get_action(self, s):
-        a, v, logp_pi = self.sess.run([self.model.pi, self.model.v, self.model.logp_pi],
-                                        feed_dict={self.model.s: s, self.model.s: s})
+    def get_action(self, s, train_mode):
+        if train_mode:
+            a, v, logp_pi = self.sess.run([self.net.pi, self.net.v, self.net.logp_pi], feed_dict={self.net.s: s, self.net.s: s})
+        else:
+            a, v, logp_pi = self.sess.run([self.net.test_pi, self.net.v, self.net.logp_pi], feed_dict={self.net.s: s, self.net.s: s})
+
         return a, v, logp_pi
 
     def save_model(self):
@@ -143,12 +147,12 @@ if __name__ == '__main__' :
     while True:
         rollout += 1
         for step in range(n_step):
-            a, v_t, logp_t = agent.get_action([s])
+            a, v_t, logp_t = agent.get_action([s], train_mode)
             a, v_t, logp_t  =  a[0], v_t[0], logp_t[0]
             env_info = env.step(a)[default_brain]
             r = env_info.rewards[0]
             d = env_info.local_done[0]
-            print(rollout, "Rollout", step+1, "Step:", "\nState:, s", "\nAction:", a, "Reward:", r, "Done:", d)
+            # print(rollout, "Rollout", step+1, "Step:", "\nState:", s, "\nAction:", a, "Reward:", r, "Done:", d)
             recent_rs.append(r)
             if step == 0:
                 s_list, a_list, v_list, d_list, r_list, logp_t_list = [s], [a], [v_t], [d], [r], [logp_t]
@@ -162,7 +166,7 @@ if __name__ == '__main__' :
 
             s = np.array(env_info.visual_observations[0])[0,:,:,:] if vis_mode else env_info.vector_observations[0]
 
-        a, v_t, logp_t = agent.get_action([s])
+        a, v_t, logp_t = agent.get_action([s], train_mode)
         v_t = v_t[0]
         v_list.append(v_t)
         next_v_list = np.copy(v_list[1:])
@@ -178,3 +182,5 @@ if __name__ == '__main__' :
         if train_mode and rollout % save_interval == 0:
             print("Model saved.")
             agent.save_model()
+
+    env.close()
